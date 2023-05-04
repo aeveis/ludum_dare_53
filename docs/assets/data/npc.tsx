5891024f1303bf22ab874068c8d797fbe95bf41e
Dan Lin <aeveis@gmail.com>
2023-05-04 02:27:36 -0400

    add game build to webpage

---
+++ docs/assets/data/npc.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="npc" tilewidth="8" tileheight="8" tilecount="2" columns="2">
+ <image source="../images/npc.png" width="16" height="8"/>
+ <tile id="0">
+  <properties>
+   <property name="name" value=""/>
+   <property name="portrait" value=""/>
+   <property name="text" value=""/>
+   <property name="type" value=""/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="name" value=""/>
+   <property name="portrait" value=""/>
+   <property name="text" value=""/>
+   <property name="type" value=""/>
+  </properties>
+ </tile>
+</tileset>
